f344587f87366385d4af2ef089824a5f51886388
FRANCISCO INACIO DOS SANTOS JUNIOR <a96198794@rn.senac.local>
2026-03-17 17:01:11 -0300

    add: Repositório da Aula de JS - Turma Full Stack - REACT

---
+++ Aula17MAR/Ex01.js
@@ -0,0 +1,113 @@
+//TODO: O condicional no javascript, você tem uma condição ?(se) valorSeForVerdadeiro :(se não) valor falso
+//! Sem Operador Ternário
+
+// let idade = 18;
+// let resultado;
+
+// if (idade >= 18){
+//     resultado ='maior de idade'
+// }else {
+//     resultado = 'Menor de idade'
+// }
+// console.log(resultado)
+
+//! Com Operador Ternário
+
+// let idade = 14;
+
+// let resultado = idade >= 18 ? "Maior de Idade" : "Menor de Idade";
+
+// console.log(resultado)
+
+//! Outro exemplo
+// //*Sem Operador Ternário
+// let numero = 15;
+// let resultado;
+
+// if (numero % 2 === 0){
+//     resultado = 'Par'
+// }else {
+//     resultado = "Ímpar"
+// }
+// console.log(resultado)
+// //? Com Operador Ternário
+// let numero = 15;
+
+// let resultado = numero % 2 ===0 ? "Par" : "Ímpar"
+
+// console.log(resultado)
+
+//! Mais Outro exemplo
+//*Operador
+// let nota = 7;
+// let situacao = nota >=6 ? 'Aprovado' : 'Reprovado';
+
+// console.log(situacao)
+
+//*If / Else
+// let nota = 5;
+
+// let situacao;
+
+// if (nota >=6){
+//     situacao = 'Aprovado'
+// }else {
+//     situacao = 'Reprovado'
+// }
+
+// console.log(situacao)
+
+//!Oto exemplo
+// let idade = 17;
+// let temCarteira = true;
+// let podeDirigir
+
+// if(idade >= 18 && temCarteira){
+//     podeDirigir = 'Autorizado'
+// }else {
+//     podeDirigir = 'tegePreso'
+// }
+
+// console.log(podeDirigir)
+// //? Operador
+// let idade = 17
+// let temCarteira = true;
+
+// let podeDirigir = idade >= 18 && temCarteira ? "Autotizado" : 'tegePreso';
+
+// console.log(podeDirigir)
+
+//TODO: ==============================================
+//TODO: ARROW FUNCTION
+
+// function somar (a,b){
+//     return a + b;
+//     }
+//* //-------------------------------------------------
+//     const somar = (a,b) =>{
+//         return a + b;
+//     }
+//! //-------------------------------------------------
+// const somar = (a,b) => a + b;
+//? //-------------------------------------------------
+// const funcao = (requisicao, resposta) = {
+//     //...
+// }
+//* //------------------------------------------------
+// const saudacao = ( ) => 'Olá';
+// console.log(saudacao())
+//! //------------------------------------------------
+// function dobro (numero){
+// return numero *2
+// }
+// console.log(dobro(8))
+
+// const dobro = numero => {
+//     return numero * 2
+// }
+// console.log(dobro(8))
+//? ----------------------------------------------------
+
+// const verificarPar = (numero) => (numero % 2 === 0 ? 'Par' : 'Ímpar')
+// console.log(verificarPar(5))
+
